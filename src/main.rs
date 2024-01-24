05052c149ed1f5fc792c116ac69403ef55b7f853
Uggla <Uggla@free.fr>
2024-01-24 16:41:13 +0100

    feat: Use a Percentage struct to avoid user errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use dojo2::{calculate_price_formated, convert_currency, Currency};
+use dojo2::{calculate_price_formated, convert_currency, Currency, Percentage};
 
 fn main() {
-    println!("{}", calculate_price_formated(5, 345, Some(10.0)));
+    println!(
+        "{}",
+        calculate_price_formated(5, 345, Some(Percentage::new(10.0)))
+    );
     println!("{}", convert_currency(20.0, Currency::Krupnic));
 }
